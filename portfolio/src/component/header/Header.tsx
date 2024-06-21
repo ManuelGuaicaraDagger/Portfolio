eb03ac66117dbeddc9efb1b85585565cb7af1a8e
ManuelGuaicaraDagger <manuelgd1997@gmail.com>
2024-06-20 21:57:58 -0300

    Pixel fixed

--- portfolio/src/component/header/Header.tsx
+++ portfolio/src/component/header/Header.tsx
@@ -2,9 +2,9 @@ function Header() {
     return (
       <header className="flex justify-between px-[200px] py-[28px] ">
         <nav className=" flex items-center">
-            <ul className="flex items-center">
+            <ul className="flex items-center w-[400px] justify-between">
                 <li className="underline-animation">Proyectos</li>
-                <li className="underline-animation mx-[80px]">Tecnologías</li>
+                <li className="underline-animation">Tecnologías</li>
                 <li className="underline-animation">Sobre mi</li>
             </ul>
         </nav>
