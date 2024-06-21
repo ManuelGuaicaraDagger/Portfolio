6dc1246a6efe846a0fcc8a3cb9163a60e58de9f9
ManuelGuaicaraDagger <manuelgd1997@gmail.com>
2024-06-20 21:47:18 -0300

    Minimal styles fixes

--- portfolio/src/component/header/Header.tsx
+++ portfolio/src/component/header/Header.tsx
@@ -3,12 +3,12 @@ function Header() {
       <header className="flex justify-between px-[200px] py-[28px] ">
         <nav className=" flex items-center">
             <ul className="flex items-center">
-                <li>Proyectos</li>
-                <li className="mx-[80px]">Tecnologías</li>
-                <li>Sobre mi</li>
+                <li className="underline-animation">Proyectos</li>
+                <li className="underline-animation mx-[80px]">Tecnologías</li>
+                <li className="underline-animation">Sobre mi</li>
             </ul>
         </nav>
-        <a className="w-[175px] h-[35px] rounded-[10px] flex justify-center items-center bg-pt-blue text-black" href="/">Contáctame</a>
+        <a className="w-[175px] h-[35px] rounded-[10px] flex justify-center items-center bg-pt-blue text-black font-bold" href="/">Contáctame</a>
       </header>
     )
   }
--- portfolio/src/component/projects/Projects.tsx
+++ portfolio/src/component/projects/Projects.tsx
@@ -2,6 +2,9 @@ function Projects() {
     return(
         <section className="flex justify-center mt-[130px]">
             <h1 className="text-[48px]">Proyectos</h1>
+            <div></div>
+            <div></div>
+            <div></div>
         </section>
     )
 }
