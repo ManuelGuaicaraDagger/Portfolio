8306680c2d9ff2624e998d89d35c73fe39b2a988
ManuelGuaicaraDagger <manuelgd1997@gmail.com>
2024-06-22 18:34:42 -0300

    CV

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './component/header/Header.tsx'
 import Main from './component/main/Main.tsx'
 import Projects from './component/projects/Projects.tsx'
+import Technologies from './component/technologies/Technologies.tsx'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
      <Header/>
      <Main/>
      <Projects/>
+     <Technologies />
     </>
   )
 }
---
+++ portfolio/src/component/technologies/Technologies.tsx
@@ -0,0 +1,19 @@
+const Technologies = () => {
+    return (
+        <section className="flex flex-col justify-center items-center mt-[60px]">
+            <div className="text-center">
+                <h1 className="text-[48px] mb-[20px] font-bold">Tecnologías</h1>
+            </div>
+            <div className="w-2/3 flex flex-wrap items-center justify-around text-2xl font-bold">
+                <div className="border">
+                    <h2>FrontEnd</h2>
+                </div>
+                <div className="border">
+                    <h2>BackEnd</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Technologies
